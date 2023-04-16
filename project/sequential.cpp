22cd499e50eff49ce8fde231e3cc27cc3a863bf6
SFARL <1084091828@qq.com>
2023-04-15 23:36:34 -0400

    fix bug

--- project/sequential.cpp
+++ project/sequential.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <chrono>
 #include <iomanip>
+#include <algorithm>
+#include <random>
 
 // sequential backtrack sudoku solution
 // Sample run ./sequential 4
@@ -12,7 +14,7 @@
 // Sample test ./sequential 16 ./16.txt ./16_ans.txt
 using namespace std;
 
-// Function to print the Sudoku grid
+// Function to prlong the Sudoku grid
 void read_grid(int *grid, int n, string filename)
 {
     ifstream file(filename);
@@ -30,7 +32,7 @@ void read_grid(int *grid, int n, string filename)
     }
 }
 
-// Function to print the Sudoku grid
+// Function to prlong the Sudoku grid
 void print_grid(int *grid, int n)
 {
     for (int i = 0; i < n * n; i++)
@@ -99,6 +101,7 @@ bool is_safe(int *grid, int n, int row, int col, int num)
 // Function to solve the Sudoku puzzle using backtracking
 bool solve_sudoku(int *grid, int n, int row, int col)
 {
+    // cout << "row: " << row << " col: " << col << endl;
     // If all rows and columns are processed, the puzzle is solved
     if (row == n - 1 && col == n)
     {
@@ -118,11 +121,13 @@ bool solve_sudoku(int *grid, int n, int row, int col)
         return solve_sudoku(grid, n, row, col + 1);
     }
     // Try to place a number in the current cell
-    int rand_num = rand() % n;
-    for (int num = 1; num <= n; num++)
+    for (int num = n; num > 0; num--)
     {
         if (is_safe(grid, n, row, col, num))
         {
+            // cout << "row: " << row << " col: " << col << " num: " << num << endl;
+            // print_grid(grid, n);
+            // cout << endl;
             grid[idx] = num;
             if (solve_sudoku(grid, n, row, col + 1))
             {
@@ -134,49 +139,30 @@ bool solve_sudoku(int *grid, int n, int row, int col)
     return false;
 }
 
-// Function to build the Sudoku puzzle using backtracking
-bool build_sudoku(int *grid, int n, int row, int col)
+void build_sudoku(int *grid, int n)
 {
-    // If all rows and columns are processed, the puzzle is solved
-
-    if (row == n - 1 && col == n)
+    std::mt19937 rng(42);
+    int root = sqrt(n);
+    for (int i = 0; i < n; i += root)
     {
-        return true;
-    }
-    // If all columns of a row are processed, move to the next row
-    if (col == n)
-    {
-        row++;
-        col = 0;
-    }
-
-    int idx = row * n + col;
-    // If the current cell is not empty, skip to the next cell
-    if (grid[idx] != 0)
-    {
-        return solve_sudoku(grid, n, row, col + 1);
-    }
-    // Try to place a number in the current cell
-    int rand_num = rand() % n;
-    for (int num = 1; num <= n; num++)
-    {
-        num = (rand_num + num) % n + 1;
-        // cout << num << endl;
-        if (is_safe(grid, n, row, col, num))
+        int num = 1;
+        for (int row = 0; row < root; row++)
         {
-            grid[idx] = num;
-            if (solve_sudoku(grid, n, row, col + 1))
+            for (int col = 0; col < root; col++)
             {
-                return true;
+                while (!is_safe(grid, n, row + i, col + i, num))
+                {
+                    num = std::uniform_int_distribution<int>{1, n}(rng);
+                }
+                grid[(row + i) * n + col + i] = num;
             }
-            grid[idx] = 0; // backtrack
         }
     }
-    return false;
+    solve_sudoku(grid, n, 0, 0);
 }
 
 // Remove numbers from the grid while ensuring the puzzle remains solvable with unique solution
-void remove(int *grid, int n, int cnt)
+void remove_unique(int *grid, int n, int cnt)
 {
     int idx;
     int row;
@@ -223,7 +209,7 @@ void remove_standard(int *grid, int n, int cnt)
         col = rand() % n;
         row = rand() % n;
         idx = row * n + col;
-        cnt--;
+
         int temp = grid[idx];
         if (temp != 0)
         {
@@ -232,21 +218,18 @@ void remove_standard(int *grid, int n, int cnt)
         }
     }
 }
+
 int main(int argc, char **argv)
 // int main_standard(int argc, char **argv)
 {
-    srand(42);
+    srand(4);
     if (argc < 2)
     {
         printf("Usage: ./sequential <int:root> \n");
         abort();
     }
-    int n = atoi(argv[1]);
-    if (sqrt(n) * sqrt(n) != n)
-    {
-        printf("Error: n must be a perfect square. \n");
-        abort();
-    }
+    int root = atoi(argv[1]);
+    int n = root * root;
     auto t_start = std::chrono::high_resolution_clock::now();
     int *grid = (int *)malloc(n * n * sizeof(int));
     // int *ans = (int *)malloc(n * n * sizeof(int));
@@ -254,7 +237,15 @@ int main(int argc, char **argv)
     {
         grid[i] = 0;
     }
-    build_sudoku(grid, n, 0, 0);
+    // int rand_num = rand() % n;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     grid[i * n + i] = (rand_num + i) % n + 1;
+    //     cout << "i: " << i << " value: " << grid[i * n + i] << endl;
+    // }
+    build_sudoku(grid, n);
+    // grid[0] = rand() % n + 1;
+    // solve_sudoku(grid, n, 0, 0);
     // for (int i = 0; i < n * n; i++)
     // {
     //     ans[i] = grid[i];
@@ -296,12 +287,8 @@ int main_unqiue(int argc, char **argv)
         printf("Usage: ./sequential <int:root> \n");
         abort();
     }
-    int n = atoi(argv[1]);
-    if (sqrt(n) * sqrt(n) != n)
-    {
-        printf("Error: n must be a perfect square. \n");
-        abort();
-    }
+    int root = atoi(argv[1]);
+    int n = root * root;
     auto t_start = std::chrono::high_resolution_clock::now();
     int *grid = (int *)malloc(n * n * sizeof(int));
     int *ans = (int *)malloc(n * n * sizeof(int));
@@ -309,13 +296,9 @@ int main_unqiue(int argc, char **argv)
     {
         grid[i] = 0;
     }
-    build_sudoku(grid, n, 0, 0);
-    for (int i = 0; i < n * n; i++)
-    {
-        ans[i] = grid[i];
-    }
+    build_sudoku(grid, n);
     int rm_cnt = n * n * 0.5;
-    remove(grid, n, rm_cnt);
+    remove_unique(grid, n, rm_cnt);
     cout << "Input grid: " << endl;
     print_grid(grid, n);
     cout << "Answer grid: " << endl;
